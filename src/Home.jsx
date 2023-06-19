485a68b66aa4903df6a8c296313d03c53f5d9367
Aryann15 <rajlysm15@gmail.com>
2023-06-19 18:14:27 +0530

    sample logic for blog mapping

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react';
 const Home = () => {
+    const [blogs, setBlogs]= useState([
+        {title: 'testing 1' , body: 'hello world...',author: 'aryan', id:1 },
+        {title: 'testing 2' , body: 'namaste world...',author: 'raj', id:2 },
+        {title: 'testing 3' , body: 'hellooo...',author: 'singh', id:3 }
+    ])
+   
   return (
-    <div>Home</div>
+    <div>
+        <h1>Homepage</h1>
+        {blogs.map((blog) => (
+            <div className="blog-preview" key ={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.authoor} </p>
+            </div>
+        ))}
+    </div>
   )
 }
 
